2ddd354a60c0170eb8288547dc7b7f7d981855ec
高橋洸允 <163371042+im-ktakahashi@users.noreply.github.com>
2025-01-09 14:15:51 +0900

    Initial commit

---
+++ src/main/java/in/tech_camp/git_app/GitAppApplication.java
@@ -0,0 +1,13 @@
+package in.tech_camp.git_app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GitAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GitAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/in/tech_camp/git_app/GitAppApplicationTests.java
@@ -0,0 +1,13 @@
+package in.tech_camp.git_app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GitAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
